227053c050256c9870a7aaf13a79ae06779dfdd9
Taanveer22 <taanveer469@gmail.com>
2025-12-12 18:56:18 +0600

    alhamdulliah login and register design done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,63 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleLoginForm = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-fit">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <form
+            onSubmit={handleLoginForm}
+            className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl"
+          >
+            <div className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 text-lg font-medium">
+        <p>New to this website please</p>
+        <button className="btn btn-sm btn-info">
+          <Link to="/register">Register</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
