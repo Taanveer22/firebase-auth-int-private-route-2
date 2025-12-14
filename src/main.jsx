4a073f80ce9a3fbaa304b7b2826bb35ed46af717
Taanveer22 <taanveer469@gmail.com>
2025-12-14 11:47:25 +0600

    alhamdulliah router v7 setup done in the project

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import Public from "./routes/Public";
 import AuthProvider from "./providers/AuthProvider";
+import publicRouter from "./routes/Public";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={Public}></RouterProvider>
+      <RouterProvider router={publicRouter} />
     </AuthProvider>
   </StrictMode>
 );
--- src/routes/Public.jsx
+++ src/routes/Public.jsx
@@ -1,47 +1,98 @@
-import { createBrowserRouter } from "react-router-dom";
+// =========== version 7 setup ============
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
 import Root from "../Root";
 import Home from "../components/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
-import Private from "./Private";
 import Orders from "../components/Orders";
 import Profile from "../components/Profile";
+import Private from "./Private";
 
-const Public = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root></Root>,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/login",
-        element: <Login></Login>,
-      },
-      {
-        path: "/register",
-        element: <Register></Register>,
-      },
-      {
-        path: "/orders",
-        element: (
-          <Private>
-            <Orders></Orders>
-          </Private>
-        ),
-      },
-      {
-        path: "/profile",
-        element: (
-          <Private>
-            <Profile></Profile>
-          </Private>
-        ),
-      },
-    ],
-  },
-]);
+const publicRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
 
-export default Public;
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+
+      <Route
+        path="orders"
+        element={
+          <Private>
+            <Orders />
+          </Private>
+        }
+      />
+
+      <Route
+        path="profile"
+        element={
+          <Private>
+            <Profile />
+          </Private>
+        }
+      />
+    </Route>
+  )
+);
+
+export default publicRouter;
+
+
+
+
+
+// ===========version 6 setup ===================
+// import { createBrowserRouter } from "react-router-dom";
+// import Root from "../Root";
+// import Home from "../components/Home";
+// import Login from "../components/Login";
+// import Register from "../components/Register";
+// import Private from "./Private";
+// import Orders from "../components/Orders";
+// import Profile from "../components/Profile";
+
+// const Public = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root></Root>,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home></Home>,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login></Login>,
+//       },
+//       {
+//         path: "/register",
+//         element: <Register></Register>,
+//       },
+//       {
+//         path: "/orders",
+//         element: (
+//           <Private>
+//             <Orders></Orders>
+//           </Private>
+//         ),
+//       },
+//       {
+//         path: "/profile",
+//         element: (
+//           <Private>
+//             <Profile></Profile>
+//           </Private>
+//         ),
+//       },
+//     ],
+//   },
+// ]);
+
+// export default Public;
